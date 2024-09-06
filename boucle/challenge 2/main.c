24b1f7e9608398bc38423bfe039357bf523061e3
Mhaaammmmmeed <mhamedaithssaine1@gmail.com>
2024-09-06 12:20:08 +0200

    challenge 2

--- boucle/challenge 2/main.c
+++ boucle/challenge 2/main.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d",&n);
     for(i=0; i<=n; i++)
     {
-        for(j=0; j<=(n*2 ); j++)
+        for(j=0; j<=(n*2 )-1; j++)
         {
             if(j>=n-i && j <= n+i)
                 printf("*");
